46f9c7558f413576cf626c358406dfde1f109505
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-07-06 11:19:38 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <InputShortener setInputValue={setInputValue}/>
+     <LinkResult inputValue={inputValue}/>
     </div>
   );
 }
